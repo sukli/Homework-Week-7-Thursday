3976910d6123c5b9911b64b86271d4f837c5923a
Suk Li <syli312@ucla.edu>
2015-02-19 06:22:25 +0000

    homework 3

---
+++ hw3.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python
+
+import pexpect
+import glob
+
+def func1(nexus_file, numgen=1000):
+	child = pexpect.spawn("mb -i " + nexus_file)
+	child.sendline("set nowarn = yes")
+	child.sendline("mcmcp ngen = " + str(numgen))
+	child.sendline("mcmc")
+	child.sendline("no")
+	child.sendline("quit")
+	child.close()
+
+
+def func2(nexus_file):
+	child = pexpect.spawn("mb -i")
+	child.sendline("execute " + nexus_file)
+	child.sendline("sumt")
+	child.expect("MrBayes >")
+	print child.before
+	child.sendline("sump")
+	child.sendline("quit")
+	child.close()
+
+
+allfiles = glob.glob("*")
+otherfiles = glob.glob("*.t")
+
+func1("primates2.nex")
+func2("primates2.nex")
+
+print "there are " + str(len(allfiles)) + " total files in the current directory and " + str(len(otherfiles)) + " that end in '.t'"
+print "these files end in '.t':"
+print otherfiles
